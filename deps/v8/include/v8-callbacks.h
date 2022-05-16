c8a9bccf73c84b5d3acc86f314f43b6be750fac5
Michaël Zasso <targos@protonmail.com>
2022-05-16 08:12:40 +0200

    deps: silence irrelevant V8 warnings

--- deps/v8/include/v8-callbacks.h
+++ deps/v8/include/v8-callbacks.h
@@ -216,9 +216,7 @@ using AddHistogramSampleCallback = void (*)(void* histogram, int sample);
 
 using FatalErrorCallback = void (*)(const char* location, const char* message);
 
-using LegacyOOMErrorCallback V8_DEPRECATE_SOON(
-    "Use OOMErrorCallback (https://crbug.com/1323177)") =
-    void (*)(const char* location, bool is_heap_oom);
+using LegacyOOMErrorCallback = void (*)(const char* location, bool is_heap_oom);
 
 struct OOMDetails {
   bool is_heap_oom = false;
@@ -323,9 +321,7 @@ using WasmSimdEnabledCallback = bool (*)(Local<Context> context);
 using WasmExceptionsEnabledCallback = bool (*)(Local<Context> context);
 
 // --- Callback for checking if WebAssembly dynamic tiering is enabled ---
-using WasmDynamicTieringEnabledCallback V8_DEPRECATE_SOON(
-    "Dynamic tiering is now enabled by default") =
-    bool (*)(Local<Context> context);
+using WasmDynamicTieringEnabledCallback = bool (*)(Local<Context> context);
 
 // --- Callback for checking if the SharedArrayBuffer constructor is enabled ---
 using SharedArrayBufferConstructorEnabledCallback =
